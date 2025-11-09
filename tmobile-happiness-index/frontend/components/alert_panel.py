2c7844806f4deb6b2230ef2a038acb8329d8c882
gjes05 <jessivgutam@gmail.com>
2025-11-09 12:21:44 -0600

    Delete tmobile-happiness-index directory

--- tmobile-happiness-index/frontend/components/alert_panel.py
+++
@@ -1,9 +0,0 @@
-from dash import html
-
-layout = html.Div([
-    html.H2("Alert Panel"),
-    html.Ul([
-        html.Li("Store 105: Happiness score dropped below 60"),
-        html.Li("Anomaly detected in voice sentiment for Store 212"),
-    ])
-])
